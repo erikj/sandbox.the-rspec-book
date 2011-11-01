0b6a7483e3206c97736a17b19e4f5acac01c1408
erikj <root@erikj.info>
2011-10-31 21:01:23 -0600

    abstracting comparisons to number_match? and exact_match?

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -7,14 +7,30 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts "Welcome to Codebreaker!"
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
-      @output.puts ''
+
+      if exact_match? guess, 0
+        mark = '+'
+      elsif number_match? guess, 0
+        mark = '-'
+      else
+        mark = ''
+      end
+      @output.puts mark
+    end
+
+    def exact_match?(guess, index)
+      guess[index] == @secret[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include? guess[index]
     end
 
   end
-
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -27,6 +27,23 @@ module Codebreaker
         end
 
       end
+      context "with 1 match" do
+        it "sends a mark of '-'" do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+
+      end
+      context "with 1 exact match" do
+        it "sends a mark of '+'" do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+
+      end
+
     end
 
   end
